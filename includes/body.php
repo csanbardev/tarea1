a0b1b47eb17d0416d06f8ec15fae3065bcee0d8e
csanbar97 <cristianbs97@gmail.com>
2022-10-18 16:38:13 +0200

    formulario implementado

--- includes/body.php
+++ includes/body.php
@@ -1,2 +1,32 @@
+<div class="container">
+  <h2>Formulario </h2>
+  <form action="validar.php" method="post">
+    <label for="nif">NIF:
+      <input class="form-control" type="text" name="nif">
+    </label>
+    <br />
+    <label for="name">Nombre:
+      <input class="form-control" type="text" name="name">
+    </label>
+    <br />
+    <label for="surname">Apellidos:
+      <input class="form-control" type="text" name="surname">
+    </label>
+    <br />
+    <label for="date">Fecha de nacimiento:
+      <input class="form-control" type="date" name="date">
+    </label>
+    <br />
+    <label for="email">Email:
+      <input class="form-control" type="email" name="email">
+    </label>
+    <br />
+    <label for="password">Contraseña:
+      <input class="form-control" type="password" name="password">
+    </label>
+    <br />
+    <input type="submit" value="Enviar" class="btn btn-success">
 
 
+  </form>
+</div>
