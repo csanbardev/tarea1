e39f47f7f7d4fc3ee4516fe27d9a2d35e1c8a8f2
csanbar97 <cristianbs97@gmail.com>
2022-10-18 16:31:32 +0200

    primer commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'includes/head.php'; ?>
+  <title>Formulario</title>
+</head>
+<body>
+  <?php include 'includes/body.php'; ?> 
+</body>
+</html>
